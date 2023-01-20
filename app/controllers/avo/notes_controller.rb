3a51bf9877af90770367fac42e01ea39415cd9d6
David Celis <me@davidcel.is>
2023-01-20 10:03:33 -0800

    Fix Avo::NotesController name

--- app/controllers/avo/notes_controller.rb
+++ app/controllers/avo/notes_controller.rb
@@ -1,4 +1,4 @@
 # This controller has been generated to enable Rails' resource routes.
 # More information on https://docs.avohq.io/2.0/controllers.html
-class Avo::ArticlesController < Avo::ResourcesController
+class Avo::NotesController < Avo::ResourcesController
 end
