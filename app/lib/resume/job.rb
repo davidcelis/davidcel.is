107f2faf49c2b67e9ac4219326bb20daaeb341b8
David Celis <me@davidcel.is>
2023-01-22 15:07:10 -0800

    Add a small work history section and downloadable CV

---
+++ app/lib/resume/job.rb
@@ -0,0 +1,20 @@
+class Resume::Job
+  include ActiveModel::API
+  attr_accessor :name, :position, :url, :startDate, :endDate, :summary
+  alias_attribute :start_date, :startDate
+  alias_attribute :end_date, :endDate
+
+  cattr_accessor :all, instance_accessor: false
+  self.all = Resume::DATA["work"].map do |job|
+    new(job).tap { |j| j.summary.squish! }
+  end
+
+  extend Enumerable
+  class << self
+    delegate :each, to: :all
+  end
+
+  def image
+    @image ||= "#{name.parameterize}.png"
+  end
+end
