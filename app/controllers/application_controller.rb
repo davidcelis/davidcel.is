d25c2d70478bfc7159d08e71b9b90a76543e54cc
David Celis <me@davidcel.is>
2023-02-01 11:45:46 -0800

    Fake twitter-like link previews for notes in Messages.app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
     cookies.encrypted[:github_user_id] == Rails.application.credentials.dig(:github, :user_id)
   end
   helper_method :authenticated?
+
+  def link_preview_requested?
+    request.env["HTTP_USER_AGENT"].match?(%r{facebookexternalhit/1.1 Facebot Twitterbot/1.0\z})
+  end
+  helper_method :link_preview_requested?
 end
