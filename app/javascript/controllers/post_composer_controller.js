a0ae519b43fdf697e7dcbc387b6a3e0510858e24
David Celis <me@davidcel.is>
2023-02-02 12:38:53 -0800

    🦣 Count characters the way the Mastodon do

--- app/javascript/controllers/post_composer_controller.js
+++ app/javascript/controllers/post_composer_controller.js
@@ -12,7 +12,20 @@ export default class extends Controller {
   }
 
   countCharacters() {
-    const textLength = this.contentTarget.textLength;
+    const rawContent = this.contentTarget.value || '';
+
+    // First, parse any URLs in the content and replace them with a placeholder
+    // so that each one properly registers as 23 characters.
+    const urlPlaceholder = 'xxxxxxxxxxxxxxxxxxxxxxx';
+    const urlRegex = /https?:\/\/[\S]+\.[\S]{2,}/g;
+    let content = rawContent.replace(urlRegex, urlPlaceholder);
+
+    // Then, parse out the domain fragment from any @mentions; for mentions like
+    // @davidcelis@xoxo.zone, the "@xoxo.zone" is not counted against the limit.
+    const mentionRegex = /(@\S+)@[\S]+\.[\S]{2,}/g;
+    content = content.replace(mentionRegex, '$1');
+
+    const textLength = content.length;
 
     this.characterCounterTarget.innerHTML = `${textLength}/${this.characterLimitValue}`;
 
