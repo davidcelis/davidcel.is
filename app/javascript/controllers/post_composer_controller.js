c8169d6765a0bb5a4234187012c80756ebab4b12
David Celis <me@davidcel.is>
2023-02-03 09:46:44 -0800

    Move re-used constants into static properties

--- app/javascript/controllers/post_composer_controller.js
+++ app/javascript/controllers/post_composer_controller.js
@@ -7,6 +7,10 @@ export default class extends Controller {
     characterLimit: { type: Number, default: 500 }
   }
 
+  static mentionRegex = /(@[\S]+)@[\S]+\.[\S]{2,}/g
+  static urlRegex = /https?:\/\/[\S]+\.[\S]{2,}/g
+  static urlPlaceholder = 'xxxxxxxxxxxxxxxxxxxxxxx'
+
   connect () {
     const options = defineOptions({
       interface: {
@@ -30,14 +34,11 @@ export default class extends Controller {
   countCharacters(rawContent) {
     // First, parse any URLs in the content and replace them with a placeholder
     // so that each one properly registers as 23 characters.
-    const urlPlaceholder = 'xxxxxxxxxxxxxxxxxxxxxxx';
-    const urlRegex = /https?:\/\/[\S]+\.[\S]{2,}/g;
-    let content = rawContent.replace(urlRegex, urlPlaceholder);
+    let content = rawContent.replace(this.constructor.urlRegex, this.constructor.urlPlaceholder);
 
     // Then, parse out the domain fragment from any @mentions; for mentions like
     // @davidcelis@xoxo.zone, the "@xoxo.zone" is not counted against the limit.
-    const mentionRegex = /(@\S+)@[\S]+\.[\S]{2,}/g;
-    content = content.replace(mentionRegex, '$1');
+    content = content.replace(this.constructor.mentionRegex, '$1');
 
     const textLength = content.length;
 
