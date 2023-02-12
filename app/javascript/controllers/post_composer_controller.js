069c4accf17db79123daa6699306539f7608abd4
David Celis <me@davidcel.is>
2023-02-12 09:55:03 -0800

    Add a media preview zone and make attachments removable

--- app/javascript/controllers/post_composer_controller.js
+++ app/javascript/controllers/post_composer_controller.js
@@ -2,8 +2,22 @@ import { Controller } from '@hotwired/stimulus'
 import { defineOptions, ink } from 'ink-mde'
 
 export default class extends Controller {
-  static targets = ['editor', 'type', 'title', 'content', 'mediaAttachments', 'characterCounter', 'dummyFileField'];
-  static values = {
+  static targets = [
+    // Post fields
+    'title',
+    'content',
+    'type',
+    'mediaAttachments',
+
+    // Utility targets
+    'editor',
+    'mediaPreviewZone',
+    'mediaPreview',
+    'characterCounter',
+    'dummyFileField'
+  ];
+
+    static values = {
     characterLimit: { type: Number, default: 500 },
     fileLimit: { type: Number, default: 4 },
   };
@@ -79,9 +93,62 @@ export default class extends Controller {
     }
 
     this.mediaAttachmentsTarget.files = dataTransfer.files;
+    this.mediaPreviewZoneTarget.innerHTML = '';
 
-    console.log(this.mediaAttachmentsTarget.files);
+    // If we have any files, unhide the media preview zone and render image or
+    // video previews for each file.
+    if (this.mediaAttachmentsTarget.files.length > 0) {
+      this.mediaPreviewZoneTarget.classList.remove('hidden');
+      this.mediaPreviewZoneTarget.classList.add('flex');
+
+      for (let i = 0; i < this.mediaAttachmentsTarget.files.length; i++) {
+        const file = this.mediaAttachmentsTarget.files[i];
+        const previewElement = this.createPreviewElementFor(file);
+
+        this.mediaPreviewZoneTarget.appendChild(previewElement);
+      }
+    }
   }
+
+  createPreviewElementFor(file) {
+    const previewElement = document.createElement('div');
+    previewElement.classList.add('relative', 'w-32', 'h-32', 'mb-2');
+
+    let mediaPreview;
+    if (file.type.startsWith('image/')) {
+      mediaPreview = document.createElement('img');
+    } else if (file.type.startsWith('video/')) {
+      mediaPreview = document.createElement('video');
+    }
+
+    mediaPreview.src = URL.createObjectURL(file);
+    mediaPreview.classList.add('object-cover', 'rounded', 'h-full', 'w-full', 'shadow-md');
+    mediaPreview.dataset.postComposerTarget = 'mediaPreview';
+    previewElement.appendChild(mediaPreview);
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('absolute', 'top-1', 'right-1', 'p-1', 'rounded-full', 'bg-black', 'bg-opacity-[.65]', 'hover:bg-black');
+    removeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 320 512"><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>'
+
+    removeButton.addEventListener('click', () => {
+      const dataTransfer = new DataTransfer();
+
+      for (let i = 0; i < this.mediaAttachmentsTarget.files.length; i++) {
+        const attachedFile = this.mediaAttachmentsTarget.files[i];
+
+        if (attachedFile.name !== file.name || attachedFile.size !== file.size || attachedFile.type !== file.type || attachedFile.lastModified !== file.lastModified) {
+          dataTransfer.items.add(attachedFile);
+        }
+      }
+
+      this.mediaAttachmentsTarget.files = dataTransfer.files;
+      previewElement.remove();
+    });
+
+    previewElement.appendChild(removeButton);
+
+    return previewElement;
+  };
 
   parseContent(rawContent) {
     // First, update the hidden content field and trim the result.
