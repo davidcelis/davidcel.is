fbce35f06ed654f7d39bfb9dd958cf9722df31eb
David Celis <me@davidcel.is>
2023-02-18 17:56:05 -0800

    🔐 Lock down direct uploads

--- app/controllers/direct_uploads_controller.rb
+++ app/controllers/direct_uploads_controller.rb
@@ -1,4 +1,6 @@
 class DirectUploadsController < ActiveStorage::DirectUploadsController
+  before_action :require_authentication
+
   private
 
   def blob_args
@@ -15,5 +17,11 @@ class DirectUploadsController < ActiveStorage::DirectUploadsController
       url: blob.service_url_for_direct_upload,
       headers: blob.service_headers_for_direct_upload.merge("X-Amz-Acl" => "public-read")
     })
+  end
+
+  def require_authentication
+    if cookies.encrypted[:github_user_id] != Rails.application.credentials.dig(:github, :user_id)
+      render json: {error: "Not authenticated"}, status: :unauthorized
+    end
   end
 end
