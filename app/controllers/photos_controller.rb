1b88f048020ed16d8960e7764784359bafa47a83
David Celis <me@davidcel.is>
2023-02-21 16:15:20 -0800

    📸 Fix sorting of photo gallery

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
-    media = MediaAttachment.featured.includes(MediaAttachment::DEFAULT_INCLUDES).order(id: :desc)
+    media = MediaAttachment.featured.includes(MediaAttachment::DEFAULT_INCLUDES).reorder(id: :desc)
     @pagy, @media_attachments = pagy(media, items: 24)
   end
 end
