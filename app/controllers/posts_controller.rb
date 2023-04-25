a5b0d05281f3b4c1fb7a70ae9950d67efbadf974
David Celis <me@davidcel.is>
2023-04-25 15:39:54 -0700

    🥔 Fix CreatePostWithMediaJob arguments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
     # of the media attachments without hogging a web worker and also ensures
     # we can wait to create the Post object until the media is done processing.
     if media_attachments_params.any?
-      CreatePostWithMediaJob.perform_async(post_params.to_h, media_attachments_params.map(&:to_h))
+      CreatePostWithMediaJob.perform_async(post_params.to_hash, media_attachments_params.map(&:to_hash))
 
       redirect_to root_path, notice: "Your post's media is being processed and will be available shortly."
     else
