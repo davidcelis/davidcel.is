76e5b3edb31e6f0f3bf49fc18ad7d8c833180807
David Celis <me@davidcel.is>
2023-08-28 18:29:15 -0700

    🗺️ Link to check-ins on Apple Maps

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
+  APPLE_MAPS_BASE_URL = "https://maps.apple.com/place".freeze
+
   has_many :check_ins
 
   validates :name, presence: true
@@ -10,5 +12,17 @@ class Place < ApplicationRecord
 
   def longitude
     coordinates.x
+  end
+
+  def apple_maps_url
+    return @apple_maps_url if defined?(@apple_maps_url)
+
+    params = {q: name, ll: [latitude, longitude].join(",")}
+    params[:auid] = apple_maps_id if apple_maps_id.present?
+
+    uri = URI(APPLE_MAPS_BASE_URL)
+    uri.query = params.to_query
+
+    @apple_maps_url = uri.to_s
   end
 end
