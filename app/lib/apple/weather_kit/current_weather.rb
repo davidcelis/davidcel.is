6ef3c59884f14046bbe2b9de91cd5677f28012ca
David Celis <me@davidcel.is>
2023-09-10 13:11:19 -0700

    🕰️ Allow pulling weather data FROM THE PAST

--- app/lib/apple/weather_kit/current_weather.rb
+++ app/lib/apple/weather_kit/current_weather.rb
@@ -3,9 +3,10 @@ module Apple
     class CurrentWeather
       BASE_URL = "https://weatherkit.apple.com/api/v1/weather/en_US".freeze
 
-      def self.at(latitude:, longitude:)
+      def self.at(latitude:, longitude:, as_of: nil)
         url = [BASE_URL, latitude, longitude].join("/")
         params = {dataSets: "currentWeather"}
+        params[:currentAsOf] = as_of.iso8601 if as_of.present?
         headers = {"Authorization" => "Bearer #{Apple::WeatherKit.token}"}
 
         HTTParty.get url, query: params, headers: headers
