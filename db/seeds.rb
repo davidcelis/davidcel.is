699d691a96a84f3c0482e2a4795528d48256721b
David Celis <me@davidcel.is>
2023-10-08 09:57:53 -0600

    🌱 Add seeds to sprout WebP variants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+MediaAttachment.includes(file_attachment: :blob, webp_variant_attachment: :blob).find_each do |media_attachment|
+  next if media_attachment.webp_variant.attached?
+
+  filename = File.basename(media_attachment.file.blob.filename.to_s, ".*")
+
+  media_attachment.file.blob.open do |file|
+    webp = ImageProcessing::Vips
+      .source(file)
+      .convert("webp")
+      .call
+
+    media_attachment.webp_variant.attach(
+      key: "blog/#{media_attachment.id}.webp",
+      io: File.open(webp.path),
+      filename: "#{filename}.webp"
+    )
+
+    puts "Created webp variant: blog/#{media_attachment.id}.webp"
+  end
+end
+
+CheckIn.includes(snapshot_attachment: :blob, webp_snapshot_attachment: :blob).find_each do |check_in|
+  next if check_in.webp_snapshot.attached?
+
+  check_in.snapshot.blob.open do |file|
+    webp = ImageProcessing::Vips
+      .source(file)
+      .convert("webp")
+      .call
+
+    check_in.webp_snapshot.attach(
+      key: "blog/snapshots/#{check_in.id}.webp",
+      io: File.open(webp.path),
+      filename: "#{check_in.id}.webp"
+    )
+
+    puts "Created webp snapshot: blog/snapshots/#{check_in.id}.webp"
+  end
+end
