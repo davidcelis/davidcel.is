0d17ad481bc820a5d85067a1e33aa233e92ec164
David Celis <me@davidcel.is>
2023-12-27 17:31:28 -0800

    🧹 Move various media attachment processing into the model

--- app/jobs/create_post_with_media_job.rb
+++ app/jobs/create_post_with_media_job.rb
@@ -34,37 +34,10 @@ class CreatePostWithMediaJob < ApplicationJob
 
       media_attachments_params.each do |blob_params|
         blob_params = blob_params.with_indifferent_access
-        media_attachment = post.media_attachments.new(
+        post.media_attachments.new(
           file: blob_params[:signed_id],
           description: blob_params.fetch(:description, "").strip.presence
         )
-
-        # If the file is a video, but not a web-friendly format, convert it to mp4.
-        # Likewise, convert GIFs to mp4 to save on space.
-        if media_attachment.gif? || (media_attachment.video? && media_attachment.file.content_type != "video/mp4")
-          convert_to_mp4(media_attachment, original_content_type: media_attachment.file.content_type)
-        end
-
-        # If the file is a video, generate and immediately attach a preview image.
-        generate_preview_image(media_attachment) if media_attachment.file.video?
-
-        # If the file is an image that was uploaded from an iPhone, it's probably
-        # in HEIC format, so we'll convert it to JPEG.
-        convert_image(media_attachment) if media_attachment.content_type == "image/heic"
-
-        # I've noticed really funky stuff happening with images that have EXIF
-        # orientation data. Uploaded images have been appearing normal in almost
-        # every location, but in very specific circumstances, the rotation seems
-        # to get repeated. The two places I've noticed this so far are iMessage
-        # link previews and RSS feed previews (specifically Reeder). When the
-        # image is embedded in HTML, it renders correctly, but when rendered
-        # independently of the HTML, it's re-rotated and looks wrong. To avoid
-        # this, we'll just manually rotate the image to the correct orientation
-        # and then strip the orientation EXIF data.
-        rotate_image(media_attachment) if media_attachment.file.image?
-
-        # Finally, if we have an image, we'll generate a WebP variant of it too.
-        generate_webp_variant(media_attachment) if media_attachment.file.image?
       end
 
       post.save!
@@ -161,119 +134,5 @@ class CreatePostWithMediaJob < ApplicationJob
       preview_image_blob.analyze
       post.preview_image.attach(preview_image_blob)
     end
-  end
-
-  def convert_to_mp4(media_attachment, original_content_type:)
-    filename = File.basename(media_attachment.file.blob.filename.to_s, ".*")
-
-    Tempfile.open([filename, ".mp4"], binmode: true) do |tempfile|
-      media_attachment.file.blob.open do |file|
-        system(ffmpeg, "-y", "-i", file.path, "-movflags", "faststart", "-pix_fmt", "yuv420p", "-vf", "scale=trunc(iw/2)*2:trunc(ih/2)*2", tempfile.path, exception: true)
-      end
-      tempfile.rewind
-
-      blob = ActiveStorage::Blob.create_and_upload!(
-        key: "blog/#{media_attachment.id}.mp4",
-        io: tempfile.to_io,
-        filename: "#{filename}.mp4",
-        metadata: {custom: {original_content_type: original_content_type}}
-      )
-
-      blob.analyze
-      media_attachment.file.attach(blob)
-    end
-  end
-
-  def generate_preview_image(media_attachment)
-    filename = File.basename(media_attachment.file.blob.filename.to_s, ".*")
-
-    Tempfile.open([filename, ".jpg"], binmode: true) do |tempfile|
-      media_attachment.file.blob.open do |file|
-        system(ffmpeg, "-y", "-i", file.path, "-vf", "select=eq(n\\,0)", "-q:v", "3", tempfile.path, exception: true)
-      end
-      tempfile.rewind
-
-      blob = ActiveStorage::Blob.create_and_upload!(
-        key: "blog/previews/#{media_attachment.id}.jpg",
-        io: tempfile.to_io,
-        filename: "#{filename}.jpg"
-      )
-
-      blob.analyze
-      media_attachment.preview_image.attach(blob)
-    end
-  end
-
-  def convert_image(media_attachment)
-    filename = File.basename(media_attachment.file.blob.filename.to_s, ".*")
-
-    media_attachment.file.blob.open do |file|
-      jpeg = ImageProcessing::Vips
-        .source(file)
-        .convert("jpeg")
-        .call
-
-      blob = ActiveStorage::Blob.create_and_upload!(
-        key: "blog/#{media_attachment.id}.jpeg",
-        io: File.open(jpeg.path),
-        filename: "#{filename}.jpeg"
-      )
-
-      blob.analyze
-      media_attachment.file.attach(blob)
-    end
-  end
-
-  def rotate_image(media_attachment)
-    filename = File.basename(media_attachment.file.blob.filename.to_s, ".*")
-    extension = Rack::Mime::MIME_TYPES.invert[media_attachment.content_type]
-
-    Tempfile.open([filename, extension], binmode: true) do |tempfile|
-      media_attachment.file.blob.open do |file|
-        image = Vips::Image.new_from_file(file.path)
-        image = image.autorot
-        image.write_to_file(tempfile.path)
-        tempfile.rewind
-      end
-
-      blob = ActiveStorage::Blob.create_and_upload!(
-        key: "blog/#{media_attachment.id}#{extension}",
-        io: tempfile.to_io,
-        filename: "#{filename}#{extension}"
-      )
-
-      blob.analyze
-      media_attachment.file.attach(blob)
-    end
-  end
-
-  def generate_webp_variant(media_attachment)
-    # If the file is already a WebP, just attach it as the webp_variant.
-    if media_attachment.content_type == "image/webp"
-      media_attachment.webp_variant.attach(media_attachment.file.blob)
-      return
-    end
-
-    filename = File.basename(media_attachment.file.blob.filename.to_s, ".*")
-
-    media_attachment.file.blob.open do |file|
-      webp = ImageProcessing::Vips
-        .source(file)
-        .convert("webp")
-        .call
-
-      blob = ActiveStorage::Blob.create_and_upload!(
-        key: "blog/#{media_attachment.id}.webp",
-        io: webp.to_io,
-        filename: "#{filename}.webp"
-      )
-
-      blob.analyze
-      media_attachment.webp_variant.attach(blob)
-    end
-  end
-
-  def ffmpeg
-    @ffmpeg ||= ActiveStorage::Previewer::VideoPreviewer.ffmpeg_path
   end
 end
--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -29,6 +29,12 @@ class MediaAttachment < ApplicationRecord
 
   scope :featured, -> { where(featured: true) }
 
+  after_create :convert_to_mp4!, if: -> { gif? || (video? && content_type != "video/mp4") }
+  after_create :generate_preview_image!, if: :video_or_gif?
+  after_create :convert_from_heic!, if: -> { content_type == "image/heic" }
+  after_create :rotate!, if: :image?
+  after_create :generate_webp_variant!, if: :image?
+
   def width
     metadata[:width]
   end
@@ -38,7 +44,7 @@ class MediaAttachment < ApplicationRecord
   end
 
   def gif?
-    custom_metadata[:original_content_type] == "image/gif"
+    content_type == "image/gif" || custom_metadata[:original_content_type] == "image/gif"
   end
 
   def video?
@@ -49,5 +55,111 @@ class MediaAttachment < ApplicationRecord
 
   def video_or_gif?
     video? || gif?
+  end
+
+  private
+
+  def convert_to_mp4!
+    filename = File.basename(file.blob.filename.to_s, ".*")
+    original_content_type = file.content_type
+
+    Tempfile.open([filename, ".mp4"], binmode: true) do |tempfile|
+      file.blob.open do |file|
+        system(ffmpeg, "-y", "-i", file.path, "-movflags", "faststart", "-pix_fmt", "yuv420p", "-vf", "scale=trunc(iw/2)*2:trunc(ih/2)*2", tempfile.path, exception: true)
+      end
+      tempfile.rewind
+
+      blob = ActiveStorage::Blob.create_and_upload!(
+        key: "blog/#{id}.mp4",
+        io: tempfile.to_io,
+        filename: "#{filename}.mp4",
+        metadata: {custom: {original_content_type: original_content_type}}
+      )
+
+      blob.analyze
+      file.attach(blob)
+    end
+  end
+
+  def generate_preview_image!
+    filename = File.basename(file.blob.filename.to_s, ".*")
+
+    Tempfile.open([filename, ".jpg"], binmode: true) do |tempfile|
+      file.blob.open do |file|
+        system(ffmpeg, "-y", "-i", file.path, "-vf", "select=eq(n\\,0)", "-q:v", "3", tempfile.path, exception: true)
+      end
+      tempfile.rewind
+
+      blob = ActiveStorage::Blob.create_and_upload!(
+        key: "blog/previews/#{id}.jpg",
+        io: tempfile.to_io,
+        filename: "#{filename}.jpg"
+      )
+
+      blob.analyze
+      preview_image.attach(blob)
+    end
+  end
+
+  def convert_from_heic!
+    filename = File.basename(file.blob.filename.to_s, ".*")
+
+    file.blob.open do |file|
+      jpeg = ImageProcessing::Vips.source(file).convert("jpeg").call
+      blob = ActiveStorage::Blob.create_and_upload!(
+        key: "blog/#{id}.jpeg",
+        io: File.open(jpeg.path),
+        filename: "#{filename}.jpeg"
+      )
+
+      blob.analyze
+      file.attach(blob)
+    end
+  end
+
+  def rotate!
+    filename = File.basename(file.blob.filename.to_s, ".*")
+    extension = Rack::Mime::MIME_TYPES.invert[content_type]
+
+    Tempfile.open([filename, extension], binmode: true) do |tempfile|
+      file.blob.open do |file|
+        image = Vips::Image.new_from_file(file.path)
+        image = image.autorot
+        image.write_to_file(tempfile.path)
+        tempfile.rewind
+      end
+
+      blob = ActiveStorage::Blob.create_and_upload!(
+        key: "blog/#{id}#{extension}",
+        io: tempfile.to_io,
+        filename: "#{filename}#{extension}"
+      )
+
+      blob.analyze
+      file.attach(blob)
+    end
+  end
+
+  def generate_webp_variant!
+    # If the file is already a WebP, just attach it as the webp_variant.
+    return webp_variant.attach(file.blob) if content_type == "image/webp"
+
+    filename = File.basename(file.blob.filename.to_s, ".*")
+
+    file.blob.open do |file|
+      webp = ImageProcessing::Vips.source(file).convert("webp").call
+      blob = ActiveStorage::Blob.create_and_upload!(
+        key: "blog/#{id}.webp",
+        io: webp.to_io,
+        filename: "#{filename}.webp"
+      )
+
+      blob.analyze
+      webp_variant.attach(blob)
+    end
+  end
+
+  def ffmpeg
+    @ffmpeg ||= ActiveStorage::Previewer::VideoPreviewer.ffmpeg_path
   end
 end
