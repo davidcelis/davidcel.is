c0b6058b71bdba0432bfb81a5f2994b0b05085ff
David Celis <me@davidcel.is>
2024-02-15 20:48:02 -0800

    🍒 With a cherry on top?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,42 @@ class ApplicationController < ActionController::Base
   def require_authentication
     redirect_to root_path unless authenticated?
   end
+
+  def track_page_view
+    plausible_client.event(
+      name: "pageview",
+      domain: plausible_domain,
+      url: request.original_url,
+      user_agent: request.user_agent,
+      referrer: request.referrer,
+      ip: request.remote_ip
+    )
+  rescue => e
+    # Don't let Plausible errors break the app, but let me know
+    Sentry.capture_exception(e)
+  end
+  helper_method :track_page_view
+
+  private
+
+  def plausible_client
+    @client ||= if Rails.env.production?
+      PlausibleApi::Client.new(plausible_domain, plausible_api_key)
+    else
+      NullPlausibleClient.new
+    end
+  end
+
+  def plausible_domain
+    @plausible_domain ||= Rails.application.credentials.dig(:plausible, :site_id)
+  end
+
+  def plausible_api_key
+    @plausible_api_key ||= Rails.application.credentials.dig(:plausible, :api_key)
+  end
+
+  class NullPlausibleClient
+    def event(*)
+    end
+  end
 end
