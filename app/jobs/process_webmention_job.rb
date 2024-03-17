fb4dc288d63b2e310147a94b1d9f9ee4bef98383
David Celis <me@davidcel.is>
2024-03-17 09:11:46 -0700

    🌉 Always verify webmentions from brid.gy

--- app/jobs/process_webmention_job.rb
+++ app/jobs/process_webmention_job.rb
@@ -2,23 +2,16 @@ class ProcessWebmentionJob < ApplicationJob
   def perform(webmention_id)
     webmention = Webmention.find(webmention_id)
 
+    # Associate the webmention with a post, if any
+    webmention.post = find_post(webmention.target)
+
     # Fetch and cache the source page
     response = Faraday.get(webmention.source)
     return webmention.failed! unless response.success?
 
+    # Save the webmention's HTML and extract its microformats2
     webmention.html = response.body
-
-    # Verify that the source page contains a link to the target
-    mentioned_urls = URI.extract(webmention.html, %w[http https])
-    return webmention.failed! unless mentioned_urls.include?(webmention.target)
-
-    # Associate the webmention with a post, if any
-    webmention.post = find_post(webmention.target)
-
-    # Extract the microformats2
     mf2 = Microformats.parse(webmention.html, base: URI.join(webmention.source, "/").to_s)
-
-    # Update the webmention
     webmention.mf2 = mf2.to_hash
 
     # For convenience, set the type of the webmention based on the microformats
@@ -34,7 +27,15 @@ class ProcessWebmentionJob < ApplicationJob
       "mention"
     end
 
-    # Mark the webmention as verified
+    # Finally, verify that the source page contains a link to the target. The
+    # sole exception is if the webmention came from brid.gy, which fails to
+    # include the target URL in its source under certain unknown conditions.
+    if URI.parse(webmention.source).host != "brid.gy"
+      mentioned_urls = URI.extract(webmention.html, %w[http https])
+      return webmention.failed! unless mentioned_urls.include?(webmention.target)
+    end
+
+    # Otherwise, mark the webmention as verified!
     webmention.verified!
   end
 
