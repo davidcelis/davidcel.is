cf3b6263006b7d404504e292707dec7211c45ddf
David Celis <me@davidcel.is>
2025-09-13 19:20:12 -0700

    💥 Prevent bad webmention URLs from triggering an exception

--- app/lib/url_validator.rb
+++ app/lib/url_validator.rb
@@ -1,7 +1,10 @@
 class URLValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless URI.parse(value).is_a?(URI::HTTP)
-      record.errors.add(attribute, options[:message] || "is not a valid URL")
+    begin
+      return if URI.parse(value).is_a?(URI::HTTP)
+    rescue URI::InvalidURIError
     end
+
+    record.errors.add(attribute, options[:message] || "is not a valid URL")
   end
 end
