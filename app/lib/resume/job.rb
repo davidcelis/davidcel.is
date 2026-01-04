5942a53c99107a247724eff72890f5ebaaef864b
David Celis <me@davidcel.is>
2026-01-04 12:06:17 -0800

    💼 Add Freshworks to the work sidebar

--- app/lib/resume/job.rb
+++ app/lib/resume/job.rb
@@ -6,6 +6,7 @@ class Resume::Job
   alias_attribute :end_date, :endDate
 
   cattr_accessor :all, instance_accessor: false
+
   self.all = Resume::DATA["work"].map do |job|
     new(job).tap { |j| j.summary.squish! }
   end
